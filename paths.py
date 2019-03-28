fe96f30af3d9e0bce25389a45b10762208571562
hyyc116 <hyyc116@gmail.com>
2019-03-28 10:34:29 +0800

    data processing

--- paths.py
+++ paths.py
@@ -11,11 +11,8 @@ class PATHS:
     def __init__(self,field):
 
         ## 领域名
-<<<<<<< HEAD
         self._DATA_FOLDER = 'DATA'
-=======
-        self._field = field
->>>>>>> 485461e8f97b7c5d433411d5f0f3b841e1ce8808
+
 
 
 
